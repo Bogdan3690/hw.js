0b8ffe7e1410927c0ce524186742f192dca276ab
Bogdan3690 <dblevko@gmail.com>
2025-07-23 14:45:19 -0600

    add first HW

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1
+const age = 16;
+console.log(age);
+// 2
+const name = "Bogdan";
+console.log(name);
+// 3
+const isStudent = true;
+console.log(isStudent);
+// 4
+const isString = true;
+console.log(isString);
+// 5
+const number = 57;
+const myNumber = number + 10;
+console.log(myNumber);
+// 6
+const myNull = null;
+console.log(myNull);
+// 7
+const userName = prompt("Введіть своє ім’я");
+alert(userName);
+console.log(typeof userName);
+// 8
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+// 9
+alert("Увага! Дія може бути небезпечною.");
+confirm("Ви впевнені, що хочете продовжити?");
