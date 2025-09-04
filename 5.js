dc493410fc73012999cea665c9206458f68bf052
Bogdan3690 <dblevko@gmail.com>
2025-09-03 18:33:32 -0600

    add hw withiout 5

---
+++ 5.js
@@ -0,0 +1,72 @@
+// let option = prompt('опції товару.').toLowerCase().trim()
+// switch(option){
+//     case 'coffee':
+//         alert('your coffee')
+//         break
+//     case 'tea':
+//         alert('your tea')
+//         break
+//     case 'juise':отдыхot
+//         alert('your juise')
+//         break
+//         default:
+//             alert('dont have this one')
+// }
+
+// let option = prompt('day?').toLowerCase().trim()
+// switch(option){
+//     case 'monday':
+//     case 'thuesday':
+//     case 'wednesday':
+//     case 'thursday':
+//     case 'friday':
+//         alert('work')
+//         break
+//     case 'saturday':
+//     case 'sunday':
+//         alert('rest')
+//         break
+//         default:
+//             alert('dont have this one')
+// }
+
+// let option = Number(prompt('What # of month?').toLowerCase().trim())
+// switch(option){
+//     case 12:
+//     case 1:
+//     case 2:
+//         alert('winter')
+//         break
+//     case 3:
+//     case 4:
+//     case 5:
+//         alert('spring')
+//         break
+//     case 6:
+//     case 7:
+//     case 8:
+//         alert('summer')
+//         break
+//     case 9:
+//     case 10:
+//     case 11:
+//         alert('autumn')
+//         break
+//         default:
+//             alert('No No No')
+// }
+
+// let option = Number(prompt('What # of month?').toLowerCase().trim())
+// switch(option){
+//     case "red":
+//         alert('stop')
+//         break
+//     case "green":
+//         alert('go')
+//         break
+//     case "yellow":
+//         alert('wait')
+//         break
+//         default:
+//             alert('No No No')
+// }
