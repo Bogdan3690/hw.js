12b3209f572294033346e52f4427d669ba5e613c
Bogdan3690 <dblevko@gmail.com>
2025-09-09 19:33:05 -0600

    add 2 problems

---
+++ 6.js
@@ -0,0 +1,19 @@
+// let i = 1
+// while (i <= 10) {
+//     console.log(i)
+//     i += 1
+// }
+
+// let seven = 7
+// let sum = 0
+// for (let i = 0; seven <= i; seven += 7) {
+//     sum += i
+// }
+//     console.log(sum);
+
+// let n = 12
+// let i = 0
+// while (i < n) {
+//     i += 1 
+//     console.log(i)
+// }
