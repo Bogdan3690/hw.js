8c1f6943988846d809854db9e6ecd4b36faff8f3
Bogdan3690 <dblevko@gmail.com>
2025-09-27 17:54:04 -0600

    add 10 problems from Telegram

---
+++ 9.js
@@ -0,0 +1,70 @@
+// let array = ['Poly', 'Ajax', 'Bob']
+// const logItems = function(number, array) {
+//     for (let i = 0; i < array.length; i++) {
+//         let numberOfElement = array.indexOf[1]
+//     }
+//     console.log(number)
+// }
+// logItems(numberOfElement, array)
+
+// function calculateEngravingPrice(message, pricePerWord) {
+    
+// }
+
+// telegram problems
+
+let sayHello = function(){
+    console.log("Hello, world!")
+}
+sayHello()
+
+let showAuthor = function(){
+    console.log('Bogdan')
+}
+showAuthor()
+
+let printCurrentYear = function(){
+    console.log(2025)
+}
+printCurrentYear()
+
+let greetUser = function(){
+    console.log("Вітаю на уроці JavaScript!")
+}
+greetUser()
+
+let printSeparator = function(){
+    console.log('**********************')
+}
+printSeparator()
+printSeparator()
+printSeparator()
+
+let sayHelloTo = function(name){
+    console.log('Hello, ' + name + '!')}
+sayHelloTo('Ira')
+
+let addNumbers = function(a, b){
+   return a + b
+}
+console.log(addNumbers(10, 5))
+console.log(addNumbers(10, 10))
+
+function printSquare(num){
+    console.log(num * num)
+}
+printSquare(5, 5)
+printSquare(10, 10)
+
+function repeatMessage(message, times){
+    for (let i = 0; i < times; i++) {
+        console.log(message, times)
+    }
+}
+repeatMessage('alo', 4)
+repeatMessage('Hey', 2)
+
+function showFullName(firstName, lastName){
+    console.log('My name is ' + firstName + ' ' + lastName)
+}
+showFullName('Bogdan', 'Levko')
